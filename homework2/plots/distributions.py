1268ec27d4e02655e4f739239a20bfdefe8baa6c
Iustina-Andreea Caramida <iustina.caramida@gmail.com>
2024-05-23 21:38:39 +0300

    modify second chapter

--- homework2/plots/distributions.py
+++ homework2/plots/distributions.py
@@ -2,62 +2,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 import os
-  
+
+def distribution_of_classes(filename_prefix):
+	filename_train = filename_prefix + '_train.csv'
+	filename_test = filename_prefix + '_test.csv'
+
+	# get the distribution of classes
+	train = pd.read_csv(filename_train)
+	test = pd.read_csv(filename_test)
+
+	last_column = train.columns[-1]
+
+	train_class_distribution = train[last_column].value_counts()
+	test_class_distribution = test[last_column].value_counts()
+
+	# plot the distribution of classes
+	plt.figure()
+	plt.bar(train_class_distribution.index, train_class_distribution.values, alpha=0.5, label='train')
+	plt.bar(test_class_distribution.index, test_class_distribution.values, alpha=0.5, label='test')
+	plt.xticks(train_class_distribution.index)
+	plt.legend()
+	plt.title(f'Distribution of classes for {os.path.basename(filename_prefix)} dataset')
+	plt.savefig(f'plots/distribution_{os.path.basename(filename_prefix)}.png', dpi=300)
+	plt.close()
 
 if __name__ == "__main__":
     # Salary Prediction datasets
-	full_set = pd.read_csv('./tema2_SalaryPrediction/SalaryPrediction_full.csv')
-	train_set = pd.read_csv('./tema2_SalaryPrediction/SalaryPrediction_train.csv')
-	test_set = pd.read_csv('./tema2_SalaryPrediction/SalaryPrediction_test.csv')
+	filename_prefix = './tema2_SalaryPrediction/SalaryPrediction'
+	distribution_of_classes(filename_prefix)
 
-	full_set_features = full_set.select_dtypes(include=['object', 'category']).columns
-	train_set_features = train_set.select_dtypes(include=['object', 'category']).columns
-	test_set_features = test_set.select_dtypes(include=['object', 'category']).columns
-
-	for feature in full_set_features:
-		plt.figure()
-		# convert nan values to string
-		full_set[feature] = full_set[feature].astype(str)
-		train_set[feature] = train_set[feature].astype(str)
-		test_set[feature] = test_set[feature].astype(str)
-
-		plt.hist(full_set[feature], bins=50, alpha=0.5, label='Full Dataset')
-		plt.hist(train_set[feature], bins=50, alpha=0.5, label='Train Dataset')
-		plt.hist(test_set[feature], bins=50, alpha=0.5, label='Test Dataset')
-
-		if len(full_set[feature].unique()) > 10:
-			plt.xticks(rotation=45)
-		
-		plt.title(f'Distribution of Salary for {feature}')
-		plt.legend()
-		plt.savefig(f'plots/distribution_{feature}_Salary_Prediction.png', dpi=300)
-		plt.close()
-	
 	# Stroke Prediction datasets
-	full_set = pd.read_csv('./tema2_AVC/AVC_full.csv')
-	train_set = pd.read_csv('./tema2_AVC/AVC_train.csv')
-	test_set = pd.read_csv('./tema2_AVC/AVC_test.csv')
-
-	full_set_features = full_set.select_dtypes(include=['object', 'category']).columns
-	train_set_features = train_set.select_dtypes(include=['object', 'category']).columns
-	test_set_features = test_set.select_dtypes(include=['object', 'category']).columns
-
-	for feature in full_set_features:
-		plt.figure()
-		# convert nan values to string
-		full_set[feature] = full_set[feature].astype(str)
-		train_set[feature] = train_set[feature].astype(str)
-		test_set[feature] = test_set[feature].astype(str)
-
-		plt.hist(full_set[feature], bins=50, alpha=0.5, label='Full Dataset')
-		plt.hist(train_set[feature], bins=50, alpha=0.5, label='Train Dataset')
-		plt.hist(test_set[feature], bins=50, alpha=0.5, label='Test Dataset')
-
-		if len(full_set[feature].unique()) > 10:
-			plt.xticks(rotation=45)
-		
-		plt.title(f'Distribution of Stroke for {feature}')
-		plt.legend()
-		plt.savefig(f'plots/distribution_{feature}_Stroke_Prediction.png', dpi=300)
-		plt.close()
+	filename_prefix = './tema2_AVC/AVC'
+	distribution_of_classes(filename_prefix)
 
